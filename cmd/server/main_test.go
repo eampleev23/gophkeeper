906677e8e8b0af418da259f9550e788517f8016f
Евгений Амплеев <eampleev@192.168.1.2>
2024-10-24 17:47:26 +0300

    test fail for save

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/eampleev23/gophkeeper/internal/server_config"
 	"github.com/eampleev23/gophkeeper/internal/services"
 	"github.com/eampleev23/gophkeeper/internal/store"
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -95,3 +95,59 @@ func TestAuthentication(t *testing.T) {
 		})
 	}
 }
+
+//
+//func TestGetLoginPass(t *testing.T) {
+//	handler := http.HandlerFunc(h.GetLoginPass)
+//	srv := httptest.NewServer(handler)
+//	defer srv.Close()
+//
+//	testCases := []struct {
+//		name         string // добавляем название тестов
+//		method       string
+//		body         string // добавляем тело запроса в табличные тесты
+//		expectedCode int
+//		expectedBody string
+//	}{
+//		{
+//			name:         "method_get",
+//			method:       http.MethodGet,
+//			expectedCode: http.StatusBadRequest,
+//			expectedBody: "",
+//		},
+//		{
+//			name:         "method_put",
+//			method:       http.MethodPut,
+//			expectedCode: http.StatusBadRequest,
+//			expectedBody: "",
+//		},
+//		{
+//			name:         "method_delete",
+//			method:       http.MethodDelete,
+//			expectedCode: http.StatusBadRequest,
+//			expectedBody: "",
+//		},
+//	}
+//	for _, tc := range testCases {
+//		t.Run(tc.method, func(t *testing.T) {
+//			req := resty.New().R()
+//			req.Method = tc.method
+//			req.URL = srv.URL
+//
+//			if len(tc.body) > 0 {
+//				req.SetHeader("Content-Type", "application/json")
+//				req.SetBody(tc.body)
+//			}
+//
+//			resp, err := req.Send()
+//			//assert.NoError(t, err, "error making HTTP request")
+//			assert.NoError(t, err, "error making HTTP request")
+//
+//			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn't match expected")
+//			// проверяем корректность полученного тела ответа, если мы его ожидаем
+//			if tc.expectedBody != "" {
+//				assert.JSONEq(t, tc.expectedBody, string(resp.Body()))
+//			}
+//		})
+//	}
+//}
--- go.mod
+++ go.mod
@@ -5,31 +5,34 @@ go 1.23.0
 require (
 	github.com/DrSmithFr/go-console v0.0.0-20240213203601-0efc7b6b93db
 	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-resty/resty/v2 v2.15.3
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/icza/gox v0.2.0
 	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
 	github.com/jackc/pgx/v5 v5.5.1
+	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.27.0
-	golang.org/x/crypto v0.21.0
-	gotest.tools v2.2.0+incompatible
+	golang.org/x/crypto v0.25.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/term v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
